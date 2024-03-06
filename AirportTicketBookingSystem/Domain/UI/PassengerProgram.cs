d8f02adc5f19e7166510247af801a087e613135c
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-03-07 01:25:51 +0200

    Implement Passenger UI

--- AirportTicketBookingSystem/Domain/UI/PassengerProgram.cs
+++ AirportTicketBookingSystem/Domain/UI/PassengerProgram.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AirportTicketBookingSystem.Domain.Models;
+using AirportTicketBookingSystem.Domain.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,36 @@ namespace AirportTicketBookingSystem.Domain.UI
 {
     internal class PassengerProgram
     {
+        internal static void AddPassenger()
+        {
+            String userInput = String.Empty;
+            String name = String.Empty;
+            Console.Clear();
+            do
+            {
+                Console.WriteLine("Passenger Name: [Enter ~ to cancel operation]");
+                userInput = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("Invalid Input\n");
+                    continue;
+                }
+                else if (userInput != "~")
+                {
+                    name = userInput;
+                }
+                else
+                {
+                    return;
+                }
+            } while (String.IsNullOrWhiteSpace(name));
+            //Create passenger
+            var passenger = new Passenger(name);
+            PassengerService.AddPassenger(passenger);
+            Console.WriteLine("\nPassenger Information Added successfully\n");
+            Console.WriteLine(passenger);
+            Console.WriteLine("\nPress enter to back");
+            Console.ReadLine();
+        }
     }
 }

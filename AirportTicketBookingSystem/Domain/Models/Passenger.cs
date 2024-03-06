1f66a4a015416e8954ef5b4cd2bacbc3505c5f4f
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-03-06 23:35:53 +0200

    Implement Passenger Model

--- AirportTicketBookingSystem/Domain/Models/Passenger.cs
+++ AirportTicketBookingSystem/Domain/Models/Passenger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,28 @@ namespace AirportTicketBookingSystem.Domain.Models
 {
     internal class Passenger
     {
+        private static int id = 1;
+        private String name = String.Empty;
+        public required int Id { get; init; }
+        public required string Name
+        {
+            get => name;
+            init
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+                name = value;
+            }
+        }
+        [SetsRequiredMembers]
+        public Passenger(string name)
+        {
+            Id = id++;
+            Name = name;
+        }
+
+        public override string ToString() => $"PassngerInfo:\nID: {Id}\nName: {Name}";
     }
 }

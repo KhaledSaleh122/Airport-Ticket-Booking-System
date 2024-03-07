d9da415197176b5efdb48861685699aaa3a20c80
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-03-07 02:49:17 +0200

    Implement Enum seat

---
+++ AirportTicketBookingSystem/Domain/Enums/Seat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.Domain.Enums
+{
+    internal enum Seat
+    {
+        Economy,
+        Business,
+        FirstClass
+    }
+}

8b81eb8524acf8aa853d505f2997a435022f97d3
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-03-08 14:42:48 +0200

    Implement Book service

--- AirportTicketBookingSystem/Domain/Services/BookService.cs
+++ AirportTicketBookingSystem/Domain/Services/BookService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AirportTicketBookingSystem.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace AirportTicketBookingSystem.Domain.Services
 {
-    internal class BookService
+    internal static class BookService
     {
+        private static Dictionary<int,Book> books = [];
+        public static void addBook(Book book) {
+            books.Add(book.Id, book);
+        }
+        public static void removeBook(Book book)
+        {
+            books.Remove(book.Id);
+        }
+
+        public static Dictionary<int, Book> getBooks() { 
+            return books.ToDictionary();
+        } 
     }
 }

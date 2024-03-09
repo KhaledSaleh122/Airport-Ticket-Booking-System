844eff6c6d838664b2c33c795b691437a2293354
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-03-09 16:27:23 +0200

    Implement Filter method for filtering books

--- AirportTicketBookingSystem/Domain/Services/BookService.cs
+++ AirportTicketBookingSystem/Domain/Services/BookService.cs
@@ -14,21 +14,50 @@ namespace AirportTicketBookingSystem.Domain.Services
             book.BookedFlight.AddPassengerToFlight(book.Class);
             books.Add(book.Id, book);
         }
-        public static void RemoveBook(Book book)
+        internal static void RemoveBook(Book book)
         {
             book.BookedFlight.RemovePassengerFromFlight(book.Class);
             books.Remove(book.Id);
         }
 
-        public static List<Book> GetBooks() { 
+        internal static List<Book> GetBooks() { 
             return books.Values.ToList();
-        }         
-        public static List<Book> GetBooks(Passenger passenger) {
+        }
+        internal static List<Book> GetBooks(Passenger passenger) {
             return books.Select(book=> book.Value).Where(book => book.Passenger == passenger).ToList();
         }
-        public static Book? GetBook(Passenger passenger,int id)
+        internal static Book? GetBook(Passenger passenger,int id)
         {
             return books.FirstOrDefault(book => book.Value.Passenger == passenger && book.Key == id).Value;
+        }
+        internal static List<Book> Filter(Dictionary<String, Object>? criterias) {
+            return books.Select((book) => book.Value).Where((value) => FilterMatch(value, criterias)).ToList();
+        }
+        private static bool FilterMatch(Book book,Dictionary<String,Object>? criterias) {
+            if (criterias is null) return true;
+            foreach (var criteria in criterias) {
+                if (criteria.Value is null) continue;
+                if (criteria.Key == "PassengerId")
+                {
+                    if (criteria.Value.GetType() == typeof(int) && (int)criteria.Value != book.Passenger.Id) return false;
+                }
+                else if (criteria.Key == "FlightId") {
+                    if (criteria.Value.GetType() == typeof(int) && (int)criteria.Value != book.BookedFlight.Id) return false;
+                }
+                else
+                {
+                    var bookProperty = book.GetType().GetProperty(criteria.Key);
+                    if (bookProperty is null)
+                    {
+                        if (!FlightService.Match(book.BookedFlight, criteria, criterias)) return false;
+                    }
+                    else
+                    {
+                        if (!object.Equals(criteria.Value, bookProperty.GetValue(book))) return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }

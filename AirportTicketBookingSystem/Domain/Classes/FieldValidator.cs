ba8e63954849f00ca0838778067070973647839d
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-04-01 17:27:59 +0300

    refactor

--- AirportTicketBookingSystem/Domain/Classes/FieldValidator.cs
+++ AirportTicketBookingSystem/Domain/Classes/FieldValidator.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace AirportTicketBookingSystem.Domain.Classes.Flight
+namespace AirportTicketBookingSystem.Domain.Classes
 {
     internal class FieldValidatorModel
     {
